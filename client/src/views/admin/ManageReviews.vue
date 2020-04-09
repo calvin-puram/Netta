2f84629d06837ea1510373b2b30ec0c44d012a3b
calvin <puram.calvin@gmail.com>
2020-04-09 14:50:06 +0100

    completed the manage review page

--- client/src/views/admin/ManageReviews.vue
+++ client/src/views/admin/ManageReviews.vue
@@ -1,8 +1,68 @@
 <template>
-  <div>
-    <h1>manage</h1>
-    <h1>manage</h1>
-    <h1>manage</h1>
-    <h1>manage</h1>
-  </div>
+  <section class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 m-auto">
+        <div class="card bg-white py-2 px-4">
+          <div class="card-body">
+            <h1 class="mb-4 text-secondary">Manage Reviews</h1>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Bootcamp</th>
+                  <th scope="col">Rating</th>
+                  <th scope="col">CreatedAt</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody v-for="bootcamp in getBootcamps" :key="bootcamp._id">
+                <tr>
+                  <td>{{ bootcamp.name }}</td>
+                  <td>{{ bootcamp.averageRating }}</td>
+                  <td>{{ new Date(bootcamp.createdAt).toDateString() }}</td>
+                  <td>
+                    <router-link
+                      :to="
+                        `/bootcamp/${bootcamp._id}/${bootcamp.slug}/createReview`
+                      "
+                    >
+                      <v-btn color="teal mr-3" dark
+                        ><BaseIcon prop="fas fa-pencil-alt"
+                      /></v-btn>
+                    </router-link>
+
+                    <router-link
+                      :to="`/bootcamp/${bootcamp._id}/${bootcamp.slug}/reviews`"
+                    >
+                      <v-btn color="teal " dark
+                        ><BaseIcon prop="fas fa-trash"
+                      /></v-btn>
+                    </router-link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import NProgress from 'nprogress';
+import store from '@store/index';
+
+export default {
+  computed: mapGetters(['getBootcamps']),
+  beforeRouteEnter(to, from, next) {
+    NProgress.start();
+    store.dispatch('getAllBootcamps').then(res => {
+      if (res && res.data.success) {
+        NProgress.done();
+      }
+    });
+    next();
+  }
+};
+</script>
