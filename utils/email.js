1034c10b8d182cff030a7c23a5d4434e358221a1
calvin <puram.calvin@gmail.com>
2020-04-13 08:28:24 +0100

    created email class

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,55 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const path = require('path');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: 2525,
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD
+module.default = class SendEmail {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = 'DevCoach <noreply@devcoach.org>';
+  }
+
+  sendTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
     }
-  });
 
-  const message = {
-    from: `${process.env.SEND_EMAIL} <${process.env.SEND_NAME}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
+    return nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 2525,
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD
+      }
+    });
+  }
 
-  await transporter.sendMail(message);
+  async send(template, subject) {
+    //render html template
+    const html = pug.renderFile(
+      path.join(__dirname, `views/emails/${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+    //email options
+    const message = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    //create transport and send email
+    await this.sendTransport().sendMail(message);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to DevCoach');
+  }
 };
-
-module.exports = sendEmail;
