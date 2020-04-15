a6b94c6f54e0dab79c918e8ed993291dece5ee22
calvin <puram.calvin@gmail.com>
2020-04-15 15:07:31 +0100

    modify the lastest card section

--- client/src/components/HomeUtils/BootcampLatest.vue
+++ client/src/components/HomeUtils/BootcampLatest.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="latest py-5 bg-light">
     <div class="container">
-      <h3 class="subtitle-5">Latest Bootcamps</h3>
+      <h3 class="subtitle-5 heading_lastest">Latest Bootcamps</h3>
       <div class="row">
         <div
           class="col-md-4 col-sm-12"
@@ -60,7 +60,7 @@
                 <div class="container">
                   <div class="row">
                     <div
-                      class="col-md-6 col-sm-12  mx-auto text-center"
+                      class="col-md-6 col-lg-3   mx-auto text-center"
                       v-for="career in bootcamp.careers"
                       :key="career"
                     >
@@ -89,5 +89,44 @@ export default {
 <style scoped>
 .card {
   height: 40rem !important;
+}
+
+/* large screen lg <= 1024px */
+@media screen and (max-width: 1024px) {
+  .card {
+    height: 45rem !important;
+
+    margin: auto;
+  }
+}
+
+/* tablet screen xs <= 768px */
+@media screen and (max-width: 768px) {
+  .latest {
+    margin-top: 8rem;
+  }
+  .heading_lastest {
+    font-style: oblique;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  .card {
+    height: 45rem !important;
+    display: block;
+    margin: auto;
+  }
+}
+/* mobile phone screen sm <= 425px */
+@media screen and (max-width: 425px) {
+  .heading_lastest {
+    font-style: oblique;
+    font-size: 20px;
+    text-align: center;
+  }
+
+  .card {
+    height: 49rem !important;
+  }
 }
 </style>
