7323e99e8aa9d2d1d1549848ee3b3fe51ad6822d
calvin <puram.calvin@gmail.com>
2020-04-16 00:12:29 +0100

    remove ratings greater than 5

--- client/src/components/BootcampsUtils/OtherFilters.vue
+++ client/src/components/BootcampsUtils/OtherFilters.vue
@@ -8,11 +8,8 @@
         @change="checkSelect"
       >
         <option value="">Please Select One</option>
-        <option value="9">9+</option>
-        <option value="8">8+</option>
-        <option value="7">7+</option>
-        <option value="6">6+</option>
-        <option value="5">5+</option>
+
+        <option value="5">5</option>
         <option value="4">4+</option>
         <option value="3">3+</option>
         <option value="2">2+</option>
