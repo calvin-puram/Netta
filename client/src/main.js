b57eab40460b58b8314a9ee0f98b61edbee97a42
calvin <puram.calvin@gmail.com>
2020-04-27 10:12:47 +0100

    setup map boilerplate

--- client/src/main.js
+++ client/src/main.js
@@ -9,6 +9,16 @@ import axios from 'axios';
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 import vuetify from './plugins/vuetify';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+
+// map icon
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
