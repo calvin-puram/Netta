b7381701aa76cd402139364e0dd48ca9798b72bd
calvin <puram.calvin@gmail.com>
2020-04-30 10:03:55 +0100

    setup basic pagination template

---
+++ client/src/components/BootcampsUtils/BootcampNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <sliding-pagination
+    :current="currentPage"
+    :total="totalPages"
+    @page-change="pageChangeHandler"
+  ></sliding-pagination>
+</template>
+
+<script>
+import SlidingPagination from 'vue-sliding-pagination';
+
+export default {
+  components: {
+    SlidingPagination
+  },
+  data() {
+    return {
+      currentPage: 1,
+      totalPages: 10
+    };
+  },
+  methods: {
+    pageChangeHandler(selectedPage) {
+      this.currentPage = selectedPage;
+    }
+  }
+};
+</script>
